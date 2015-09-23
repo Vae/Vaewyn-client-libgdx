f271fd7c7f8a855c6c81e1519018ec55b11fb5a9
protocol <DataProtocol@gmail.com>
2015-09-23 18:21:36 -0500

    master
    Change-Id: I70da97b27ca25cfd55ecd7906e834325f8437e0f

---
+++ my-gdx-game-core/src/com/mygdx/game/Area.java
@@ -0,0 +1,9 @@
+package com.mygdx.game;
+
+public class Area {
+	public Area() {
+	}
+
+	public void render() {
+	}
+}
---
+++ my-gdx-game-core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,93 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+//import com.badlogic.gdx.scenes.scene2d.ui.Label;
+//import com.badlogic.gdx.scenes.scene2d.ui.Table;
+//import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+
+public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
+	SpriteBatch batch;
+	Texture img;
+
+	@Override
+	public void create() {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+
+		// Label nameLabel = new Label("Name:", skin);
+		// TextField nameText = new TextField("", skin);
+		// Label addressLabel = new Label("Address:", skin);
+		// TextField addressText = new TextField("", skin);
+
+		// Table table = new Table();
+		// table.add(nameLabel);
+		// table.add(nameText).width(100);
+		// table.row();
+		// table.add(addressLabel);
+		// table.add(addressText).width(100);
+
+		Gdx.input.setInputProcessor(this);
+	}
+
+	@Override
+	public void render() {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		if (Gdx.input.isKeyPressed(Keys.ESCAPE))
+			Gdx.app.exit();
+		return true;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
